a69a530b9dadedcada17d074c795e4df42831768
Joël Pittet <ph06ilj@eduvaud.ch>
2023-09-08 15:46:32 +0200

    Version avec liste et random

--- MasterMind/Program.cs
+++ MasterMind/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("+----------+");
+Console.WriteLine("|MasterMind|");
+Console.WriteLine("+----------+");
+
+int NbColor = 0;
+string RandomColorName;
+string UserAnswer;
+
+var ColorList = new List<string>();
+ColorList.Add("White");
+ColorList.Add("Red");
+ColorList.Add("Green");
+ColorList.Add("Blue");
+ColorList.Add("Yellow");
+ColorList.Add("Cyan");
+ColorList.Add("Magenta");
+ColorList.Add("Gray");
+
+
+do
+{
+    Random RandomCode = new Random();
+    int RandomColor = RandomCode.Next(ColorList.Count);
+    NbColor++;
+
+    Console.WriteLine(ColorList[RandomColor]);
+
+} while (NbColor != 5);
+
+
+
+
+Console.WriteLine("Quel est le code mystère selon vous ?");
+UserAnswer = Console.ReadLine();
