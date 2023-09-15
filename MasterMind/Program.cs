712aeb4a32068c5b0d7243467a83514e5ca5403c
Joël Pittet <ph06ilj@eduvaud.ch>
2023-09-15 14:28:14 +0200

    AvecComparaisonDeBonneCouleurs

--- MasterMind/Program.cs
+++ MasterMind/Program.cs
@@ -1,34 +1,85 @@
-﻿Console.WriteLine("+----------+");
+﻿//
+int NbColor = 0;
+string UserAnswer;
+int GoodColor = 0;
+
+//Liste pour couleurs disponibles
+var ColorList = new List<string>();
+ColorList.Add("rouge");
+ColorList.Add("vert");
+ColorList.Add("bleu");
+ColorList.Add("orange");
+ColorList.Add("jaune");
+ColorList.Add("violet");
+ColorList.Add("rose");
+
+//Liste pour code secret
+var ColorSelected = new List<string>();
+
+
+Console.WriteLine("+----------+");
 Console.WriteLine("|MasterMind|");
 Console.WriteLine("+----------+");
 
-int NbColor = 0;
-string RandomColorName;
-string UserAnswer;
+Console.WriteLine("Bienvenue dans le MasterMind");
+Console.WriteLine("Les couleurs disponibles sont les suivantes: rouge, vert, bleu, orange, jaune, violet, rose");
 
-var ColorList = new List<string>();
-ColorList.Add("White");
-ColorList.Add("Red");
-ColorList.Add("Green");
-ColorList.Add("Blue");
-ColorList.Add("Yellow");
-ColorList.Add("Cyan");
-ColorList.Add("Magenta");
-ColorList.Add("Gray");
-
-
+//Boucle pour la création du code grâce à ColorList
 do
 {
     Random RandomCode = new Random();
     int RandomColor = RandomCode.Next(ColorList.Count);
+
+    ColorSelected.Add(ColorList[RandomColor]);
+
     NbColor++;
 
-    Console.WriteLine(ColorList[RandomColor]);
+    Console.WriteLine($"{NbColor}. {ColorList[RandomColor]}");
 
-} while (NbColor != 5);
-
+} while (NbColor != 4);
 
 
 
 Console.WriteLine("Quel est le code mystère selon vous ?");
-UserAnswer = Console.ReadLine();
+Console.WriteLine("Réponse sous ce format: vert,jaune,vert,jaune");
+
+//Récupération de la réponse utilisateur
+UserAnswer = Console.ReadLine().ToLower();
+
+
+//Si la réponse utilisateur est égale au code secret affiche un message de victoire
+if (UserAnswer == $"{ColorSelected[0]},{ColorSelected[1]},{ColorSelected[2]},{ColorSelected[3]}")
+{
+
+    Console.WriteLine("Vous avez trouvé le code secret, Bravo !!");
+
+}
+else // Sinon compare chaque réponse pour voir s'il y en a des bonnes, lui affiche le nombre de juste
+{
+    var userColors = UserAnswer.Split(",");
+
+    for (var i = 0; i < userColors.Length; i++)
+    {
+
+        if (userColors[i] == ColorSelected[i])
+        {
+
+            GoodColor++;
+
+        }
+
+
+        //Console.WriteLine($"{userColors[i]} --  {ColorSelected[i]}");
+    }
+
+    Console.WriteLine($"Vous avez {GoodColor} bonnes couleurs aux bons endroits");
+
+
+
+
+}
+
+
+
+
+
