2a606390580bf2e24b2d5b3993dc71bb5d8df852
Joël Pittet <ph06ilj@eduvaud.ch>
2023-10-06 15:49:06 +0200

    Fini mais manque peaufinage

--- MasterMind/Program.cs
+++ MasterMind/Program.cs
@@ -10,7 +10,7 @@ int GoodColorPlace = 0;
 int GoodColor = 0;
 int ColorUserCount = 1;
 int tries = 1;
-var LColorUser = new List <string>();
+var userGuess = new List <string>();
 
 
 //Liste pour couleurs disponibles
@@ -24,42 +24,43 @@ ColorList.Add("violet");
 ColorList.Add("rose");
 
 //Liste pour code secret
-var LColorSelected = new List<string>();
+var secretCode = new List<string>();
 
 
-Console.WriteLine("+----------+");
-Console.WriteLine("|MasterMind|");
-Console.WriteLine("+----------+");
+Console.WriteLine("+--------------+");
+Console.WriteLine("|  MasterMind  |");
+Console.WriteLine("+--------------+");
 
-Console.WriteLine("Bienvenue dans le MasterMind");
-Console.WriteLine("Les couleurs disponibles sont les suivantes: rouge, vert, bleu, orange, jaune, violet, rose");
+Console.WriteLine("Bienvenue dans le MasterMind\n\n");
+Console.WriteLine("Les couleurs disponibles sont les suivantes: rouge, vert, bleu, orange, jaune, violet, rose\n");
 
-    //Boucle pour la création du code grâce à ColorList
-    do
-    {
-        Random RandomCode = new Random();
-        int RandomColor = RandomCode.Next(ColorList.Count);
+//Boucle pour la création du code grâce à ColorList
+do
+{
+    Random RandomCode = new Random();
+    int RandomColor = RandomCode.Next(ColorList.Count);
 
-        LColorSelected.Add(ColorList[RandomColor]);
+    secretCode.Add(ColorList[RandomColor]);
 
 
-        NbColor++;
+    NbColor++;
 
-        Console.WriteLine($"{NbColor}. {ColorList[RandomColor]}");
+    //Console.WriteLine($"{NbColor}. {ColorList[RandomColor]}");
 
-    } while (NbColor != 4);
+} while (NbColor != 4);
 
 
 do
 {
-    Console.WriteLine("Quel est le code mystère selon vous ?");
+    Console.WriteLine("\nQuel est le code mystère selon vous ?\n");
 
     //Réinitialise le compteur pour qu'il y ai toujours la suite 1. 2. 3. 4. quand l'utilisateur rentre les couleurs
     ColorUserCount = 1;
     GoodColorPlace = 0;
     GoodColor = 0;
-
-    Console.WriteLine($"Essai numéro {tries}");
+    //Reinitialise la list pour que l'index n'aille pas au dessus de 4
+    userGuess = new List<string>();
+    Console.WriteLine($"Essai numéro {tries}\n");
 
     //demande, récupère et insère dans un tableau les couleurs de l'utilisateur
     for (int i = 0; i < 4; i++)
@@ -67,63 +68,63 @@ do
         
         Console.Write($"Couleur {ColorUserCount}: ");
         ColorUserCount++;
-        LColorUser.Add(Console.ReadLine().ToLower());
+        userGuess.Add(Console.ReadLine().ToLower());
 
 
     }
 
     tries++;
 
-    //Copie de la liste du code secret pour pouvoir retirer les couleurs juste sans problème
+    //Copie de la liste de couleurs de l'utilisateur pour pouvoir retirer les couleurs juste sans problème
     //et pouvoir gérer les couleurs juste au mauvais endroit
-    var LColorSelectedCopy = LColorSelected.ToList();
-    var LColorUserCopy = LColorUser.ToList();
+    var secretCodeCopy = secretCode.ToList();
+    var userGuessCopy = userGuess.ToList();
 
     //compare les couleurs du code secret et de l'utilisateur 
 
-    for (int i = 0; i < LColorUser.Count; i++)
+    for (int i = 0; i < userGuess.Count; i++)
     {
         //Ajoute 1 à chaque couleur donnée qui correspond au code secret 
-        if (LColorUser[i] == LColorSelected[i])
+        if (userGuess[i] == secretCode[i])
         {
 
             GoodColorPlace++;
 
             //enlève les couleurs trouvées pour compter les couleurs
-            //justes au mauvais endroits 
-            LColorSelectedCopy.Remove(LColorUser[i]);
-            LColorUserCopy.Remove(LColorUser[i]);
-        
+            //justes aux mauvais endroits 
+            secretCodeCopy.Remove(userGuess[i]);
+            userGuessCopy.Remove(userGuess[i]);
+
         }
 
     }
-    //Check dans la liste du code si elle contient des couleurs données
-    for (int i = 0; i < LColorUser.Count; i++)
+
+    //Vérifie dans la liste du code si elle contient des couleurs données
+    for (int i = 0; i < userGuessCopy.Count; i++)
     {
-        if (LColorSelectedCopy.Contains(LColorUserCopy[i]))
+        if (secretCodeCopy.Contains(userGuessCopy[i]))
         {
-
             GoodColor++;
-
         }
     }
 
+ 
+    
 
     //Retourne à l'utilisateur le nombre de bonne couleurs et le nombre de couleurs juste au mauvais endroit
-    Console.WriteLine($"Vous avez {GoodColorPlace} bonnes couleurs aux bons endroits");
+    Console.WriteLine($"\nVous avez {GoodColorPlace} bonnes couleurs aux bons endroits");
    Console.WriteLine($"Vous avez {GoodColor} couleurs juste en mauvaise position");
 
 
-}while (tries <= 10);
+}while (GoodColorPlace != 4 && tries <= 10);
 
 
-
-
-/*if (GoodColorPlace == 4)
+//Message de victoire et de défaite
+if (GoodColorPlace == 4)
 {
     Console.WriteLine("Vous avez trouvé le code secret, Bravo !!");
 
-}else if (tries == 10)
+}else if (tries > 10)
 {
-    Console.WriteLine("Vous avez atteint le nombre d'essais maximum, le code secret était");
-}*/
+    Console.WriteLine($"Vous avez atteint le nombre d'essais maximum, le code secret était: {secretCode[0]} {secretCode[1]} {secretCode[2]} {secretCode[3]}");
+}
